798837d617cf6272bf1d18a72f8628837771d532
itought <mountyjapan24@gmail.com>
2025-11-18 15:34:26 +0200

    ini commit

---
+++ Location/Location/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Location
+{
+    public partial class Form1 : Form
+    {
+        //int x = 2480;
+        //int y = 1375;
+        //Button button = new Button();
+        //Button button2 = new Button();
+        //Button button3 = new Button();
+        //Button button4 = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //button1.Name = "Left_Button";
+            //button1.Text = "←";
+            //button1.Size = new Size(40, 40);
+            //button1.Click += Left_Click;
+            //button1.Location = new Point(300, 500);
+            //this.Controls.Add(button1);
+
+            //button2.Name = "Right_Button";
+            //button2.Text = "→";
+            //button2.Size = new Size(40, 40);
+            //button2.Click += Right_Click;
+            //button2.Location = new Point(400, 500);
+            //this.Controls.Add(button2);
+
+            //button3.Name = "Up_Button";
+            //button3.Text = "↑";
+            //button3.Size = new Size(40, 40);
+            //button3.Click += Up_Click;
+            //button3.Location = new Point(350, 450);
+            //this.Controls.Add(button3);
+
+            //button4.Name = "Down_Button";
+            //button4.Text = "↓";
+            //button4.Size = new Size(40, 40);
+            //button4.Click += Down_Click;
+            //button4.Location = new Point(350, 500);
+            //this.Controls.Add(button4);
+        //    button.Name = "Create";
+        //    button.Text = "Create";
+        //    button.Size = new Size(70, 40);
+        //    button.Click += Create_Click;
+        //    button.Location = new Point(x, y);
+        //    this.Controls.Add(button);
+        //}
+
+        //int count = 0;
+        //private void Create_Click(object sender, EventArgs e)
+        //{
+        //    Random random = new Random();
+
+        //    int color1 = random.Next(0, 255);
+        //    int color2 = random.Next(0, 255);
+        //    int color3 = random.Next(0, 255);
+        //    if (count == 0)
+        //    {
+        //        Button buttonCreate = new Button();
+        //        buttonCreate.Name = "Create";
+        //        buttonCreate.Text = "B" + color1;
+        //        buttonCreate.Size = new Size(70, 40);
+        //        buttonCreate.BackColor = Color.FromArgb(color1, color2, color3);
+        //        buttonCreate.Click += Create_Click;
+        //        buttonCreate.Location = new Point(x - 80, this.ClientSize.Width);
+        //        this.Controls.Add(buttonCreate);
+        //        x -= 80;
+        //        count++;
+        //        if (x < 0)
+        //        {
+        //            x = 2560;
+        //            y -= 50;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Button buttonCreate = new Button();
+
+
+        //        buttonCreate.Name = "Create";
+        //        buttonCreate.Text = "B" + color1;
+        //        buttonCreate.Size = new Size(70, 40);
+        //        buttonCreate.BackColor = Color.FromArgb(color1, color2, color3);
+        //        buttonCreate.Click += Create_Click;
+        //        buttonCreate.Location = new Point(x - 80, y);
+        //        this.Controls.Add(buttonCreate);
+        //        x -= 80;
+        //        if(x < 0)
+        //        {
+        //            x = 2560;
+        //            y -= 50;
+        //        }
+        //        count = 0;
+        //    }
+        }
+    }
+}
